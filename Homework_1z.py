ca03c0d21cfdacfbb40bbca23490e7ed5c7b1540
Delovar1994 <ivan-kuricev@yandex.ru>
2019-04-05 13:23:03 +0300

    Тест гита

---
+++ Homework_1z.py
@@ -0,0 +1,9 @@
+a = 100
+b = 'сто'
+c = 99.9
+py_true = True
+list_1 = [1, 77, 3, 5, 9, 'проверить']
+print(a, b, c, py_true, list_1[5])
+d = int(input('Введите целое число'))
+list_1[5] = d
+print('Вы ввели', list_1[5])
